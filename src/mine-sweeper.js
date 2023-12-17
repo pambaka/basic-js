785c979c15e68ad70a877c2b9614345b7887ff63
pambaka <pambaka@ya.ru>
2023-12-17 11:20:06 +0100

    feat: mine-sweeper is implemented

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const counters = [];
+  for (let r = 0; r < matrix.length; r += 1) {
+    counters[r] = [];
+    for (let c = 0; c < matrix[r].length; c +=1) {
+      counters[r][c] = 0;
+    }
+  }
+  const rows = matrix.length;
+  const columns = matrix[0].length; // suppose all arrays have the same length
+  
+  function isValid (x, y) {
+    if (x >= 0 && x < columns && y >= 0 && y < rows) {
+      return true;
+    }
+    return false;
+  }
+
+  for (let r = 0; r < rows; r += 1) {
+    for (let c = 0; c < columns; c += 1) {
+      for (let y = r - 1; y <= r + 1; y += 1) {
+        for (let x = c - 1; x <= c + 1; x += 1) {
+          if (isValid(x, y) && (x !== c || y !== r)) {
+            if (matrix[y][x] === true) {
+              counters[r][c] += 1;
+            }
+          }  
+        }
+      }
+    }
+  }
+
+  return counters;
 }
 
 module.exports = {
