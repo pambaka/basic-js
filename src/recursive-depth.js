c5c6c1dd8135599bf0ab4e5a08485a0107f96de2
pambaka <pambaka@ya.ru>
2023-12-24 13:27:12 +0100

    feat: recursive-depth is implemented

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,10 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+  calculateDepth(arr) {
+    let counter = 0;
+
+    if (Array.isArray(arr)) {
+      for (let i = 0; i < arr.length; i += 1) {
+        counter = Math.max(counter, this.calculateDepth(arr[i]));
+      }
+      counter += 1;
+    }
+
+    return counter;
+  }  
 }
 
 module.exports = {
