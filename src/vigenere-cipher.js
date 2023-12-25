57837dc1fd42c7a6315786a68556b66eccd0c758
pambaka <pambaka@ya.ru>
2023-12-25 21:34:09 +0100

    feat: vigenere-cipher is implemented

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,100 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (value = true) {
+    this.isDirect = value;
+
+    const N = 26; // number of letters
+    this.firstLetterCode = 65;
+    this.letters = [];
+    this.re = /[A-Z]/;
+
+    for (let i = 0; i < N; i += 1) {
+      this.letters[i] = String.fromCharCode((this.firstLetterCode + i) % N + this.firstLetterCode); 
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, value) {
+    if (!str || !value) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let upperStr = str.toUpperCase();
+    let upperValue = value.toUpperCase();
+    let encLetter = ''; 
+    let encStr = '';
+    let key = '';
+    let shift = 0;
+
+    for (let i = 0; i < str.length; i += 1) {
+      if (String(upperStr[i]).match(this.re)) {
+        key += upperValue[(i - shift) % value.length];
+      } else {
+        key += ' ';
+        shift += 1;
+      }
+    }
+    
+    for (let i = 0; i < str.length; i += 1) {
+      if (String(upperStr[i]).match(this.re)) {
+      const charCode = (this.letters.indexOf(upperStr[i]) + 
+        this.letters.indexOf(key[i])) % this.letters.length + this.firstLetterCode;
+      
+      encLetter = String.fromCodePoint(charCode);
+      encStr += encLetter;
+      }
+      else encStr += upperStr[i];
+    }
+
+    if (!this.isDirect) {
+      let reversedStr = encStr.split('').reverse().join('');
+      
+      return reversedStr;
+    }
+    
+    return encStr;
+  }
+
+  decrypt(str, value) {
+    if (!str || !value) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let upperStr = str.toUpperCase();
+    let upperValue = value.toUpperCase();
+    let decLetter = '';
+    let decStr = '';
+    let key = '';
+
+    let shift = 0;
+    for (let i = 0; i < str.length; i += 1) {
+      if (String(upperStr[i]).match(this.re)) {
+        key += upperValue[(i - shift) % value.length];
+      } else {
+        key += ' ';
+        shift += 1;
+      }
+    }
+
+    for (let i = 0; i < str.length; i += 1) {
+      if (String(upperStr[i]).match(this.re)) {
+        const charCode = (this.letters.indexOf(upperStr[i]) - this.letters.indexOf(key[i]) 
+          + this.letters.length) % this.letters.length + this.firstLetterCode;
+        
+        decLetter = String.fromCodePoint(charCode);
+        decStr += decLetter;
+      } else {
+        decStr += upperStr[i];
+      }
+    }
+
+    if (!this.isDirect) {
+      let reversedStr = decStr.split('').reverse().join('');
+      
+      return reversedStr;
+      }
+
+    return decStr;
   }
 }
 
